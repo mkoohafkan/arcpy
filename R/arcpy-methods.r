52d50f9589edc96b58ccb1be3e3c5f924e8ad748
mkoohafkan <michael.koohafkan@gmail.com>
2018-09-19 12:58:14 -0700

    full suite of operators with documentation

--- R/arcpy-methods.r
+++ R/arcpy-methods.r
@@ -1,8 +1,41 @@
 ops = reticulate::import("operator", delay_load = TRUE)
 
+#' Arcpy Raster Algebra 
+#'
+#' Operators for arcpy raster algebra.
+#'
+#' @param a,b arcpy raster objects.
+#' @param n non-negative integer.
+#' 
+#' @details The following operators are supported:
+#' 
+#' Arithmetic Operators: 
+#' * `+`, `-`, and`/`
+#' * `**` and `^`
+#' * `%%` and `%/%`
+#'
+#' Logical Operators:
+#' * `!`, `&`, `|`, and `xor`
+#' * `!=` and `==`
+#' * `<`, `<=`, `>`, and `>=`
+#'
+#' Bitwise Operators
+#' * `bitwNot`, `bitwAnd`, `bitwOr` and `bitwXor`
+#' * `bitwShiftL` and `bitwShiftR` 
+#'
+#' @name RasterAlgebra
+#' @seealso \code{\link[base]{Arithmetic}}, \code{\link[base]{Comparison}}, 
+#'  \code{\link[base]{bitwNot}}.
+NULL
+
+# arithmetic operators
+
 #' @export
 "+.python.builtin.Raster" <- function(a, b) {
-  ops$add(a, b)
+  if (missing(b))
+    ops$pos(a)
+  else
+    ops$add(a, b)
 }
 
 #' @export
@@ -11,7 +44,8 @@ ops = reticulate::import("operator", delay_load = TRUE)
     ops$neg(a)
   else
     ops$sub(a, b)
-}
+  }
+
 
 #' @export
 "*.python.builtin.Raster" <- function(a, b) {
@@ -27,6 +61,24 @@ ops = reticulate::import("operator", delay_load = TRUE)
 "^.python.builtin.Raster" <- function(a, b) {
   ops$pow(a, b)
 }
+
+#' @export
+"**.python.builtin.Raster" <- function(a, b) {
+  ops$pow(a, b)
+}
+
+#' @export
+"%/%.python.builtin.Raster" <- function(a, b) {
+  ops$floordiv(a, b)
+}
+
+#' @export
+"%%.python.builtin.Raster" <- function(a, b) {
+  ops$mod(a, b)
+}
+
+
+# logical operators
 
 #' @export
 "<.python.builtin.Raster" <- function(a, b) {
@@ -65,20 +117,48 @@ ops = reticulate::import("operator", delay_load = TRUE)
 
 #' @export
 "&.python.builtin.Raster" <- function(a, b) {
-  ops$and(a, b)
+  ops$and_(a, b)
 }
 
 #' @export
 "|.python.builtin.Raster" <- function(a, b) {
-  ops$or(a, b)
+  ops$or_(a, b)
 }
 
 #' @export
-"%/%.python.builtin.Raster" <- function(a, b) {
-  ops$floordiv(a, b)
+xor.python.builtin.Raster <- function(a, b) {
+  ops$xor(a, b)
+}
+
+
+# bitwise operators
+
+#' @export
+bitwNot.python.builtin.Raster <- function(a) {
+  ops$invert(a)
 }
 
 #' @export
-"%%.python.builtin.Raster" <- function(a, b) {
-  ops$mod(a, b)
+bitwAnd.python.builtin.Raster <- function(a, b) {
+  ops$and_(a, b)
+}
+
+#' @export
+bitwOr.python.builtin.Raster <- function(a, b) {
+  ops$or_(a, b)
+}
+
+#' @export
+bitwXor.python.builtin.Raster <- function(a, b) {
+  ops$xor_(a, b)
+}
+
+#' @export
+bitwShiftL.python.builtin.Raster <- function(a, n) {
+  ops$lshift(a, n)
+}
+
+#' @export
+bitwShiftR.python.builtin.Raster <- function(a, n) {
+  ops$rshift(a, n)
 }

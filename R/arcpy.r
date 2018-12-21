d4387b3520cff91495778501da1d56a02f5ea674
mkoohafkan <michael.koohafkan@gmail.com>
2018-12-21 14:19:19 -0800

    fixed documentation

--- R/arcpy.r
+++ R/arcpy.r
@@ -1,12 +1,11 @@
 #' R Interface to ArcPy
 #'
-#' An R interface for the ArcGIS Python module \code{arcpy}. Relies on 
-#' the virtual Python environment provided by \code{PythonInR} to generate 
-#' function interfaces for \code{arcpy} tools and environment settings.
-#' \code{arcpy} toolboxes and extensions are accessed via an R environment
-#' object. See the vignette to get started.
-#' @name arcpyr-package
-#' @aliases arcpyr
+#' An R interface for the ArcGIS Python module \code{arcpy} via the 
+#' R-Python interface provided by reticulate. See the vignettes to get
+#' started.
+#'
+#' @name arcpy-package
+#' @aliases arcpy
 #' @docType package
 #' @md
 #' @seealso \code{\link{use_ArcGIS}}

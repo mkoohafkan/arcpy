159480f805dfc7bf5f030cb01d5afb2800bca3c2
Michael C Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-04-02 17:35:46 -0700

    updated vignettes

--- _build_vignettes.r
+++ _build_vignettes.r
@@ -1,7 +1,8 @@
 devtools::load_all()
-use_ArcGIS(TRUE)
+use_arcpy()
 
 knitr::knit("vignettes/_quickstart.rmd", "vignettes/quickstart.rmd")
 knitr::knit("vignettes/_helper-functions.rmd", "vignettes/helper-functions.rmd")
 knitr::knit("vignettes/_raster-math.rmd", "vignettes/raster-math.rmd")
-knitr::knit("vignettes/_manipulating-arcobjects.rmd", "vignettes/manipulating-arcobjects.rmd")
+knitr::knit("vignettes/_manipulating-arcobjects.rmd",
+  "vignettes/manipulating-arcobjects.rmd")

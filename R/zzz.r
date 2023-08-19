efb774eb663118703b6f93c27457b181e53167cc
Michael C Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-08-19 10:20:55 -0700

    cleaner import on load

--- R/zzz.r
+++ R/zzz.r
@@ -1,8 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   # import modules
-  arcpy <<- reticulate::import("arcpy", delay_load = list(priority = 1,
+  arcpy <<- reticulate::import("arcpy", delay_load = list(priority = 5,
     environment = "r-arcpy"))
-  arcgis <<- reticulate::import("arcgis", delay_load = list(priority = 2,
+  arcgis <<- reticulate::import("arcgis", delay_load = list(priority = 10,
     environment = "r-arcpy"))
   # look for ArcGIS Pro by default if option not defined
   invisible(TRUE)

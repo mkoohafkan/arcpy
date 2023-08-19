c94900b9f964a27efca10cb46703bfa46cdedf8f
Michael C Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-08-19 10:20:37 -0700

    reexport some reticulate helpers

---
+++ R/reexports.r
@@ -0,0 +1,22 @@
+#' @importFrom reticulate import
+#' @export
+reticulate::import
+
+#' @importFrom reticulate iterate
+#' @export
+reticulate::iterate
+
+#' @importFrom reticulate as_iterator
+#' @export
+reticulate::as_iterator
+
+#' @importFrom reticulate iter_next
+#' @export
+reticulate::iter_next
+
+#' @export
+reticulate::`%as%`
+
+#' @importFrom reticulate use_condaenv
+#' @export
+reticulate::use_condaenv

40ea39777461d08104939b211d1b2a004422c86b
Michael Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2023-12-21 08:25:02 -0800

    doc updates

--- R/zzz.r
+++ R/zzz.r
@@ -4,6 +4,5 @@
     environment = "r-arcpy"))
   arcgis <<- reticulate::import("arcgis", delay_load = list(priority = 10,
     environment = "r-arcpy"))
-  # look for ArcGIS Pro by default if option not defined
   invisible(TRUE)
 }

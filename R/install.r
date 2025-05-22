b56eeec5a163127f4e895cbd84f1cbde026197bd
Michael Koohafkan <Michael.C.Koohafkan@usace.army.mil>
2025-05-22 08:03:10 -0700

    fixes #8

--- R/install.r
+++ R/install.r
@@ -19,17 +19,16 @@ arcpy_version = function(version, conda = "auto", channel = "esri", forge = TRUE
   # get all modules matching specified version
   arcpy_selected = arcpy_available[arcpy_available$version == version,]
   if (nrow(arcpy_selected) < 1L) {
-      stop("No arcpy module matching version ", version, " found.")
+    stop("No arcpy module matching version ", version, " found.")
   }
   # get latest build
-  arcpy_selected = arcpy_selected[order(arcpy_selected$build),]
-  arcpy_selected = arcpy_selected[nrow(arcpy_selected),]
-  arcpy_version = arcpy_selected$version
+  which.version = order(arcpy_selected$build, decreasing = TRUE)[1]
+  arcpy_version = arcpy_selected$version[which.version]
   # get python version
-  arcpy_build = arcpy_selected$build
+  arcpy_build = arcpy_selected$build[which.version]
   python_version = regmatches(arcpy_build,
-    regexpr("(?<=py)([0-9]{2})(?=_arcgispro_[0-9]+)",
-    arcpy_build, perl = TRUE))
+    regexpr("(?<=py)([0-9]{2,})(?=_arcgispro_[0-9]+)",
+      arcpy_build, perl = TRUE))
   python_version = gsub("([0-9]{1})([0-9]{1})", "\\1.\\2", python_version)
   list(arcpy = arcpy_version, python = python_version)
 }
